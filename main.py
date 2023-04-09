c5c02c138822f22629e31387d77499987899231d
hyaroy_pg <>
2023-04-09 09:39:02 +0900

    add requirements.txt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import discord
+from discord.ext import tasks
 from dotenv import load_dotenv
 import requests
 import json
@@ -35,6 +36,9 @@ def call_api(utterance):
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send('CHAPPiE has connected to Discord!')
+    # send_periodic_message.start()
 
 
 @client.event
@@ -43,9 +47,16 @@ async def on_message(message):
         return
 
     if client.user in message.mentions:
-        print(f"{message.content}")
+        print(f"{message.content}")  # メンションの内容
         response = call_api(message.content)
-        print(f"{response}")
+        print(f"{response['bestResponse']['utterance']}")  # botからのレスポンスの内容
         await message.reply(response['bestResponse']['utterance'])
+
+
+@tasks.loop(seconds=15)
+async def send_periodic_message():
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send(str("定期発話だよ"))
+
 
 client.run(BOT_TOKEN)
